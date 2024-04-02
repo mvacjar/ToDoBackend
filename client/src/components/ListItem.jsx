3a627b00f3e16777c920bdd01a6c539fd104b8a9
mvacjar <mvacjar@gmail.com>
2024-04-02 14:17:28 +0200

    Media queries Add

--- client/src/components/ListItem.jsx
+++ client/src/components/ListItem.jsx
@@ -1,9 +1,9 @@
-import { useState } from 'react';
-import ProgressBar from './ProgressBar';
-import TickIcon from './TickIcon';
-import Modal from './Modal';
-import '../styles/App.css';
-import '../styles/ListItem.css';
+import { useState } from "react";
+import ProgressBar from "./ProgressBar";
+import TickIcon from "./TickIcon";
+import Modal from "./Modal";
+import "../styles/App.css";
+import "../styles/ListItem.css";
 const serverUrl = import.meta.env.VITE_SERVER_URL;
 
 function ListItem({ task, getData }) {
@@ -12,7 +12,7 @@ function ListItem({ task, getData }) {
   const deleteItem = async () => {
     try {
       const response = await fetch(`${serverUrl}/todos/${task.id}`, {
-        method: 'DELETE',
+        method: "DELETE",
       });
       if (response.status === 200) {
         getData();
@@ -23,26 +23,28 @@ function ListItem({ task, getData }) {
   };
 
   return (
-    <div className='listItem-container'>
-      <div className='wrapper'>
-        <div className='info-container'>
-          <TickIcon />
-          <p className='task-title'>{task.title}</p>
+    <div className="listItem-container">
+      <div className="wrapper">
+        <div className="data-container">
+          <div className="info-container">
+            <TickIcon />
+            <p className="task-title">{task.title}</p>
+          </div>
+          <div className="progressBar-container">
+            <ProgressBar progress={task.progress} />
+          </div>
         </div>
-        <div className='progressBar-container'>
-          <ProgressBar progress={task.progress} />
-        </div>
-        <div className='button-container'>
-          <button className='edit-button' onClick={() => setShowModal(true)}>
+        <div className="button-container">
+          <button className="edit-button" onClick={() => setShowModal(true)}>
             Edit
           </button>
-          <button className='delete-button' onClick={deleteItem}>
+          <button className="delete-button" onClick={deleteItem}>
             Delete
           </button>
         </div>
         {showModal && (
           <Modal
-            mode={'edit'}
+            mode={"edit"}
             setShowModal={setShowModal}
             task={task}
             getData={getData}
